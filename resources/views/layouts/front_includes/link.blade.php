2e9777922487b0834b27cdd74ae3ad241282aa1b
raju-sah <rajucode7@gmail.com>
2026-02-03 00:10:34 +0545

    refactor: reordered stylesheet link imports in `link.blade.php`.

--- resources/views/layouts/front_includes/link.blade.php
+++ resources/views/layouts/front_includes/link.blade.php
@@ -35,6 +35,24 @@
     }
 </script>
 
+{{-- Main Stylesheet --}}
+<link rel="stylesheet" href="{{ asset('style.css') }}">
+
+{{-- Google Fonts --}}
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,100&family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+{{-- Bootstrap --}}
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+{{-- Font Awesome --}}
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+{{-- Swiper --}}
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+
 {{-- Global Theme Transition CSS --}}
 <style>
     /* Smooth theme transitions for ALL elements */
@@ -169,23 +187,5 @@
         backdrop-filter: blur(10px);
     }
 </style>
-
-{{-- Main Stylesheet --}}
-<link rel="stylesheet" href="{{ asset('style.css') }}">
-
-{{-- Google Fonts --}}
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,100&family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-
-{{-- Bootstrap --}}
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-{{-- Font Awesome --}}
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-
-{{-- Swiper --}}
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 
 @stack('front_css')
